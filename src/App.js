21340eaf02e8808f9f7ba43120406fc42fc7ef88
Marcus <marcuspilkington@outlook.com>
2025-01-21 23:47:58 +0000

    init commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={"/marcus_and_nicole.jpeg"} className="App-logo" alt="Save the date!" />
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
